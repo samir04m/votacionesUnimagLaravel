5ad3259e7210a8e4cb58cc71fd35da37c9b8d686
Samir Mejia <samir4mt@gmail.com>
2018-05-08 02:19:42 -0500

    Cambio a secure_asset

--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -5,12 +5,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>@yield('title', '') - Votaciones Unimag 2018</title>
 
-	<link rel="stylesheet" href="{{ asset('css/materialize.css') }}"">
-	<link rel="stylesheet" href="{{ asset('css/estilos.css') }}"">
-	<link rel="stylesheet" href="{{ asset('css/material-icons.css') }}"">
+	<link rel="stylesheet" href="{{ secure_asset('css/materialize.css') }}"">
+	<link rel="stylesheet" href="{{ secure_asset('css/estilos.css') }}"">
+	<link rel="stylesheet" href="{{ secure_asset('css/material-icons.css') }}"">
 	<style>
 		body {
-			background-image: url("{{ asset('imagenes/patron.png') }}");
+			background-image: url("{{ secure_asset('imagenes/patron.png') }}");
 		}
 	</style>
 </head>
@@ -26,9 +26,9 @@
 
 	@yield('extrabody', '')
 
-	<script type="text/javascript" src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('js/materialize.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('js/myScript.js') }}"></script>
+	<script type="text/javascript" src="{{ secure_asset('js/jquery-3.3.1.min.js') }}"></script>
+	<script type="text/javascript" src="{{ secure_asset('js/materialize.js') }}"></script>
+	<script type="text/javascript" src="{{ secure_asset('js/myScript.js') }}"></script>
 	
 
 	@yield('extrajs', '')
